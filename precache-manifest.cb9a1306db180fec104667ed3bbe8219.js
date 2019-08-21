1c71eee6a4a2b958a77f375edf16bf3544fc5e35
vgcps4n <prizo.crow@gmail.com>
2019-08-21 13:03:23 +0800

    Updates

---
+++ precache-manifest.cb9a1306db180fec104667ed3bbe8219.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e2699148d9aeb1135b2d5fa5ba3a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfcf38238352ebf6f7b0",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "28393a107b04825455d8",
+    "url": "/static/css/main.6d27706b.chunk.css"
+  },
+  {
+    "revision": "dfcf38238352ebf6f7b0",
+    "url": "/static/js/2.e6abde3e.chunk.js"
+  },
+  {
+    "revision": "28393a107b04825455d8",
+    "url": "/static/js/main.f6827502.chunk.js"
+  },
+  {
+    "revision": "1f53c752473104352e66",
+    "url": "/static/js/runtime~main.5d4ae4cf.js"
+  },
+  {
+    "revision": "8b69af64cfbc4527fa1f431a1c8bb888",
+    "url": "/static/media/bg.8b69af64.jpg"
+  },
+  {
+    "revision": "fef593c92fe590629d305f301824356a",
+    "url": "/static/media/logo.fef593c9.png"
+  },
+  {
+    "revision": "e1f5f591d0b488518b5a38d5dbad8e22",
+    "url": "/static/media/logopic.e1f5f591.png"
+  },
+  {
+    "revision": "d3eaf389e048b72efe4330ad9c36506b",
+    "url": "/static/media/logoword.d3eaf389.png"
+  },
+  {
+    "revision": "adb442a55a1471b049535b9d876fc1ca",
+    "url": "/static/media/logowordblack.adb442a5.png"
+  }
+]);
